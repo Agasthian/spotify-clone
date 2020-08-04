eead43500e9ff65065083efd4a9ae7c7571c4512
Agasthian <agasthian_r@hotmail.com>
2020-08-04 18:54:41 +0530

    Created the login page with spotify authentication and compeleted setting up the react context API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+import Login from './components/Login';
+import Player from './components/Player';
+import { getTokenFromUrl } from './api/spotify';
+import { useDataLayerValue } from './DataLayer';
+
+//instanciate spotify web api
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //state
+  const [token, setToken] = useState(null);
+  const [{}, dispatch] = useDataLayerValue();
+
+  //runs the code on given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token); // gives the token to the imported spotify api
+
+      spotify.getMe().then((user) => {
+        console.log('👱', user); //GetMe is an built function of spotify webapi js library
+      });
+    }
+
+    console.log('i have a token', token);
+  }, []);
+
+  return <div className='app'>{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
